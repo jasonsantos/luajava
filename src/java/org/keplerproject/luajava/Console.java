5436be06ac0b40f23fd314a145b4b0f93f8feaf6
thiago <thiago>
2005-04-01 00:49:52 +0000

    Console now uses 'load' functions, instead of 'do' functions

--- src/java/org/keplerproject/luajava/Console.java
+++ src/java/org/keplerproject/luajava/Console.java
@@ -1,25 +1,25 @@
 /*
-* Copyright (C) 2004 Kepler Project.
-*
-* Permission is hereby granted, free of charge, to any person obtaining
-* a copy of this software and associated documentation files (the
-* "Software"), to deal in the Software without restriction, including
-* without limitation the rights to use, copy, modify, merge, publish,
-* distribute, sublicense, and/or sell copies of the Software, and to
-* permit persons to whom the Software is furnished to do so, subject to
-* the following conditions:
-*
-* The above copyright notice and this permission notice shall be
-* included in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ * Copyright (C) 2004 Kepler Project.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 package org.keplerproject.luajava;
 
@@ -28,97 +28,73 @@ import java.io.InputStreamReader;
 
 /**
  * LuaJava console
+ * 
  * @author Thiago Ponte
  */
 public class Console
 {
 
-  /**
-   * Creates a console for user interaction
-   * @param args - names of the lua files to be executed
-   */
-	public static void main(String[] args)
-	{
-		try
-		{
-			LuaState L = LuaStateFactory.newLuaState();
-			L.openBasicLibraries();
-			L.openDebug();
-			L.openLoadLib();
+   /**
+    * Creates a console for user interaction
+    * 
+    * @param args -
+    *           names of the lua files to be executed
+    */
+   public static void main(String[] args)
+   {
+      try
+      {
+         LuaState L = LuaStateFactory.newLuaState();
+         L.openBasicLibraries();
+         L.openDebug();
+         L.openLoadLib();
 
-			if (args.length > 0)
-			{
-				for (int i = 0; i < args.length; i++)
-				{
-					int res = L.doFile(args[i]);
-					if (res != 0)
-					{
-					  String str;
-			      if (res == LuaState.LUA_ERRRUN.intValue())
-			      {
-			        str = "Runtime error. ";
-			      }
-			      else if (res == LuaState.LUA_ERRMEM.intValue())
-			      {
-			        str = "Memory allocation error. ";
-			      }
-			      else if (res == LuaState.LUA_ERRERR.intValue())
-			      {
-			        str = "Error while running the error handler function. ";
-			      }
-			      else
-			      {
-			        str = "Lua Error code " + res + ". ";
-			      }
+         if (args.length > 0)
+         {
+            for (int i = 0; i < args.length; i++)
+            {
+               int res = L.LloadFile(args[i]);
+               if (res == 0)
+               {
+                  res = L.pcall(0, 0, 0);
+               }
+               if (res != 0)
+               {
+                  throw new LuaException("Error on file: " + args[i] + ". " + L.toString(-1));
+               }
+            }
 
-					  throw new LuaException(str + "Error on file " + args[i]);
-					}
-				}
+            return;
+         }
 
-				return;
-			}
+         System.out.println("API Lua Java - console mode.");
 
-			System.out.println("API Lua Java - console mode.");
+         BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
 
-			BufferedReader inp =
-				new BufferedReader(new InputStreamReader(System.in));
+         String line;
 
-			String line;
+         System.out.print("> ");
+         while ((line = inp.readLine()) != null && !line.equals("exit"))
+         {
+            int ret = L.LloadBuffer(line.getBytes(), "from console");
+            if (ret == 0)
+            {
+               ret = L.pcall(0, 0, 0);
+            }
+            if (ret != 0)
+            {
+               System.err.println("Error on line: " + line);
+               System.err.println(L.toString(-1));
+            }
+            System.out.print("> ");
+         }
 
-		  System.out.print("> ");
-			while ((line = inp.readLine()) != null && !line.equals("exit"))
-			{
-				int ret = L.doString(line);
-				if (ret != 0)
-				{
-				  String str;
-		      if (ret == LuaState.LUA_ERRRUN.intValue())
-		      {
-		        str = "Runtime error. ";
-		      }
-		      else if (ret == LuaState.LUA_ERRMEM.intValue())
-		      {
-		        str = "Memory allocation error. ";
-		      }
-		      else if (ret == LuaState.LUA_ERRERR.intValue())
-		      {
-		        str = "Error while running the error handler function. ";
-		      }
-		      else
-		      {
-		        str = "Lua Error code " + ret + ". ";
-		      }
-					System.out.println(str + line);
-				}
-			  System.out.print("> ");
-			}
-			
-			L.close();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
+         L.close();
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+      }
 
-	}
+   }
 }
