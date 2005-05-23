70ab652849dc73c0c533113b3d4722e946a3feda
tuler <tuler>
2005-05-23 15:48:52 +0000

    copyright year update

--- src/java/org/keplerproject/luajava/LuaObject.java
+++ src/java/org/keplerproject/luajava/LuaObject.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2004 Kepler Project.
+* Copyright (C) 2005 Kepler Project.
 *
 * Permission is hereby granted, free of charge, to any person obtaining
 * a copy of this software and associated documentation files (the
