6a79fdb6dd414e88a2c1604fac4de514432bc096
Tianwei Du <duzc2dtw@gmail.com>
2015-01-02 14:07:28 +0800

    Update LuaState.java

--- src/java/org/keplerproject/luajava/LuaState.java
+++ src/java/org/keplerproject/luajava/LuaState.java
@@ -33,7 +33,7 @@ package org.keplerproject.luajava;
  */
 public class LuaState
 {
-  private final static String LUAJAVA_LIB = "luajava-1.1";
+  private final static String LUAJAVA_LIB = "/luajava-1.1";
 
   final public static Integer LUA_GLOBALSINDEX  = new Integer(-10002);
   final public static Integer LUA_REGISTRYINDEX = new Integer(-10000);
@@ -83,7 +83,19 @@ public class LuaState
    */
   static
   {
-    System.loadLibrary(LUAJAVA_LIB);
+	String arch = System.getProperty("os.arch");
+    try{
+		System.loadLibrary(arch+"/lua51");
+	}catch(Throwable e){
+		e.printStackTrace();
+		System.err.println("catched");
+	}
+    try{
+		System.loadLibrary(System.getProperty("os.arch")+LUAJAVA_LIB);
+	}catch(Throwable e){
+		e.printStackTrace();
+		System.err.println("catched");
+	}
   }
 
   private CPtr luaState;
